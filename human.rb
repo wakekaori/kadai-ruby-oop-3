c68a989962e68d66258f3961ceb8f62c539e220e
wakekaori <wakekaoridevelopment@gmail.com>
2023-05-04 14:22:27 +0000

    new

---
+++ human.rb
@@ -0,0 +1,14 @@
+require "./animal"
+require "./thinkable"
+
+class Human < Animal
+  attr_accessor :hobby
+  
+  def initialize(name, age, hobby)
+    super(name, age)
+    self.hobby = hobby
+  end
+
+  include Thinkable
+  
+end
